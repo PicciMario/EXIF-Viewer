d79e625bee3ec181ae9115ff1e541f0526c10d6c
PicciMario <mario.piccinelli@gmail.com>
2011-08-26 12:13:42 +0200

    started working on filesystem data

--- exifreporter.py
+++ exifreporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from exifviewer import ExifData
-import sys, math
+import sys, math, os, hashlib
 
 import PIL, urllib, cStringIO
 from PIL import ImageDraw
@@ -113,6 +113,23 @@ Story = []
 
 # ------- Header Section ----------------------------------------------------
 
+def fileMd5(original_filename):
+	try:
+		f = file(original_filename ,'rb')
+		
+		md5 = hashlib.md5()
+		while True:
+			data = f.read()
+			if not data:
+				break
+			md5.update(data)
+		md5String = md5.hexdigest()
+		
+		f.close()
+		return md5String
+	except:
+		return ""
+
 Story.append(Paragraph("Image analysis report", styles["Title"]))
 
 thumbWidth = 3*inch
@@ -122,6 +139,18 @@ im = Image(filename, thumbWidth, thumbHeight)
 
 imgData = Table(
 	[
+		[
+			Paragraph("File name:", styles["Small"]),
+			Paragraph("%s"%filename, styles["Small"])
+		],
+		[
+			Paragraph("File size:", styles["Small"]),
+			Paragraph("%s kB"%(os.path.getsize(filename)/1024), styles["Small"])
+		],
+		[
+			Paragraph("File MD5:", styles["Small"]),
+			Paragraph("%s"%fileMd5(filename), styles["Small"])
+		],
 		[
 			Paragraph("Image format", styles["Small"]), 
 			Paragraph(exifData.imageFormat, styles["Small"])
@@ -133,15 +162,83 @@ imgData = Table(
 		[
 			Paragraph("Image size", styles["Small"]), 
 			Paragraph("%s px"%exifData.imageSize, styles["Small"])
-		]
+		],	
+
 	],
-	colWidths=[94, 210]
+	colWidths=[70, 234]
 )
 imgData.setStyle(tableStyleStandard)
 
 t=Table([[im, imgData]], colWidths=[3*inch+6, 310])
 t.setStyle(tableStyleImg)
 Story.append(t)
+
+Story.append(Spacer(10, 20))
+
+# ------- FileSystem Section ---------------------------------------------
+
+Story.append(Paragraph("FileSystem data", styles['Heading2']))
+
+stats = os.stat(filename)
+
+osData = [
+	[
+		Paragraph("Protection bits", styles["Small"]), 
+		Paragraph("%s"%bin(stats.st_mode), styles["Small"])
+	],
+	[
+		Paragraph("Inode number", styles["Small"]), 
+		Paragraph("%s"%stats.st_ino, styles["Small"])
+	],
+	[
+		Paragraph("Device", styles["Small"]), 
+		Paragraph("%s"%stats.st_dev, styles["Small"])
+	],
+	[
+		Paragraph("Number of hard links", styles["Small"]), 
+		Paragraph("%s"%stats.st_nlink, styles["Small"])
+	],
+	[
+		Paragraph("UID of the owner", styles["Small"]), 
+		Paragraph("%s"%stats.st_uid, styles["Small"])
+	],
+	[
+		Paragraph("GID of the owner", styles["Small"]), 
+		Paragraph("%s"%stats.st_gid, styles["Small"])
+	],
+	[
+		Paragraph("Time of most recent access", styles["Small"]), 
+		Paragraph("%s"%stats.st_atime, styles["Small"])
+	],
+	[
+		Paragraph("Time of most recent content modification", styles["Small"]), 
+		Paragraph("%s"%stats.st_mtime, styles["Small"])
+	],
+	[
+		Paragraph("Time of most recent metadata change (time of creation in Windows systems)", styles["Small"]), 
+		Paragraph("%s"%stats.st_ctime, styles["Small"])
+	]
+]
+
+if (hasattr(stats, "st_blocks")):
+	osData.append(
+		[
+			Paragraph("Number of blocks allocated", styles["Small"]), 
+			Paragraph("%s"%stats.st_blocks, styles["Small"])
+		]
+	)
+
+if (hasattr(stats, "st_blksize")):
+	osData.append(
+		[
+			Paragraph("Filesystem block size", styles["Small"]), 
+			Paragraph("%s"%stats.st_blksize, styles["Small"])
+		]
+	)
+
+osDataTable = Table(osData, colWidths=[150, 380])
+osDataTable.setStyle(tableStyleStandard)
+Story.append(osDataTable)
 
 Story.append(Spacer(10, 20))
 
@@ -202,7 +299,6 @@ def gpsImg(filename, lat, lon, zoom):
 	except:
 		print("Unable to download image for GPS data from OpenStreetMap")
 		return 1
-	
 
 gpsData = exifData.decodeGpsData(exifData.getGpsData())
 
